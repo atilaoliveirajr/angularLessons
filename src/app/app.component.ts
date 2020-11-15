d05fe41e8dcfb7d8cc600db9b2eb761293dd6bea
atilaoliveirajr <atila.oliveira.jr@gmail.com>
2020-11-15 22:12:56 +0100

    Added images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'myApp';
+export class AppComponent implements OnInit {
+  imagesArray = ['pic1.png', 'pic2.jpeg', 'pic3.jpeg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.imagesArray.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 2000);
+  }
 }
