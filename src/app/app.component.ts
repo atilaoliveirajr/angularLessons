c27acce82fab20599fea19909704dcc65b6cca2e
atilaoliveirajr <atila.oliveira.jr@gmail.com>
2020-11-16 09:53:49 +0100

    Started header menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,25 +5,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  imagesArray = ['pic1.png', 'pic2.jpeg', 'pic3.jpeg'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.imagesArray.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 2000);
-  }
+export class AppComponent  {
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  imagesArray = ['pic1.png', 'pic2.jpeg', 'pic3.jpeg'];
+  headlinesArray = [
+    'Bring engineering to the next level',
+    'Born to code',
+    'Graduated at TUM'
+];
+  
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.imagesArray.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 2000);
+  }
+}
