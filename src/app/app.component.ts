129c117c98fa4532f3e45af692b41bd61473a4f3
atilaoliveirajr <atila.oliveira.jr@gmail.com>
2020-11-22 13:29:12 +0100

    final adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent /* implements OnInit */ {
+
+  /* correntRounte = ''; */
+  constructor(public router: Router) { }
+
+  /* ngOnInit() {
+    this.correntRounte = this.router.url;
+    console.log(this.router.url);
+  } */
+  
 }
